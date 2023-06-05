14af136828cea8b26d7685c744aca7d8c6841d82
framgo <lucasaraujooks@gmail.com>
2023-06-04 23:20:31 -0300

    feat: fazendo o repositorio dos filmes

---
+++ repository/FilmeRepository.php
@@ -0,0 +1,107 @@
+<?php
+    require_once('Connection.php');
+
+    function fnAddFilme($titulo, $diretor, $duracao, $categoria, $sumario, $capa, $nota){
+        $con = getConnection();
+        $sql = "insert into filme (titulo, diretor, duracao, categoria, sumario, capa, nota) values (:pTitulo, :pDiretor, :pDuracao, :pCategoria, :pSumario, :pCapa, :pNota)";
+        $stmt = $con->prepare($sql);
+
+        $stmt->bindParam(":pTitulo", $titulo);
+        $stmt->bindParam(":pDiretor", $diretor);
+        $stmt->bindParam(":pDuracao", $duracao);
+        $stmt->bindParam(":pCategoria", $categoria);
+        $stmt->bindParam(":pSumario", $sumario);
+        $stmt->bindParam(":pCapa", $capa);
+        $stmt->bindParam(":pNota", $nota);
+
+        return $stmt->execute();
+    }
+
+    function fnListFilmes(){
+        $con = getConnection();
+        $sql = "select * from filme";
+        $result = $con->query($sql);
+        $lstFilmes = array();
+
+        while($filme = $result->fetch(PDO::FETCH_OBJ)){
+            array_push($lstFilmes, $filme);
+        }
+
+        return $lstFilmes;
+
+    }
+
+    function fnLocalizaFilmePorTitulo($titulo){
+        $con = getConnection();
+        $sql = "select * from filme where titulo like :pTitulo limit 20";
+        $stmt = $con->prepare($sql);
+        $stmt->bindValue(":pTitulo", "%{$titulo}%");
+
+        if($stmt->execute()) {
+            $stmt->setFetchMode(PDO::FETCH_OBJ);
+            return $stmt->fetchAll();
+        }
+    }
+
+    function fnLocalizaFilmePorCategoria($categoria){
+        $con = getConnection();
+        $sql = "select * from filme where categoria like :pCategoria limit 20";
+        $stmt = $con->prepare($sql);
+        $stmt->bindValue(":pCategoria", "%{$categoria}%");
+
+        if($stmt->execute()) {
+            $stmt->setFetchMode(PDO::FETCH_OBJ);
+            return $stmt->fetchAll();
+        }
+    }
+
+    function fnLocalizaFilmePorId($id){
+        $con = getConnection();
+        $sql = "select * from filme where id = :pID";
+        
+        $stmt = $con->prepare($sql);
+        $stmt->bindParam(":pID", $id);
+        
+        if($stmt->execute()){
+            return $stmt->fetch(PDO::FETCH_OBJ);
+        }
+        return null;
+    }
+
+    function fnUpdateFilme($id, $titulo, $diretor, $duracao, $categoria, $sumario, $nota){
+        $con = getConnection();
+        $sql = "update filme set titulo = :pTitulo, diretor = :pDiretor, duracao = :pDuracao, categoria = :pCategoria, sumario = :pSumario, nota = :pNota where id = :pID";
+
+        $stmt = $con->prepare($sql);
+        $stmt->bindParam(":pID", $id);
+        $stmt->bindParam(":pTitulo", $titulo);
+        $stmt->bindParam(":pDiretor", $diretor);
+        $stmt->bindParam(":pDuracao", $duracao);
+        $stmt->bindParam(":pCategoria", $categoria);
+        $stmt->bindParam(":pSumario", $sumario);
+        $stmt->bindParam(":pNota", $nota);
+
+        return $stmt->execute();
+    }
+
+    function fnDeleteManga($id){
+        $con = getConnection();
+        $sql = "delete from filme where id = :pID";
+        
+        $stmt = $con->prepare($sql);
+        $stmt->bindParam(":pID", $id);
+
+        return $stmt->execute();
+    }
+
+    
+    function fnUpdateNota($nota) {
+        $con = getConnection();
+        $sql = "update filme set nota = :Nota where id = :pID";
+        
+        $stmt = $con->prepare($sql);
+        $stmt->bindParam(":pID", $id);
+        $stmt->bindParam(":Nota", $nota);
+
+        return $stmt->execute();
+    }
